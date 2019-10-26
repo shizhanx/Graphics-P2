c4b3a350ca554e8c604f6cee6f412d52ada0316b
shizhanx <43192138+shizhanx@users.noreply.github.com>
2019-10-26 18:27:41 +1100

    Add files via upload (#14)

---
+++ Assets/Scripts/DestroyWhenParticlesFinished.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWhenParticlesFinished : MonoBehaviour
+{
+    public ParticleSystem targetParticleSystem;
+
+    void Update()
+    {
+        if (!this.targetParticleSystem.IsAlive())
+        {
+            Destroy(targetParticleSystem.gameObject);
+        }
+    }
+}
